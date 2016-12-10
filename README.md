2c0751cceeb65041b055fcaf031b11372e571c16
Stefano Diem Benatti <stefano@heavenstudio.com.br>
2016-12-09 22:28:45 -0200

    allowing cli to download songs using its name/youtube search

--- README.md
+++ README.md
@@ -4,23 +4,25 @@ A ruby script to download the audio stream of any kind of youtube video without 
 It will bypass copyright restrictions, VEVO accounts...
 
 ## Needed Gems
-- nokogiri
-- open-uri
-- youtube-dl.rb  https://github.com/layer8x/youtube-dl.rb
+- bundler
+- gems bundled in Gemfile
+
+## Install
+clone the repo and run `bundle install`
 
 ## Commands
+Download a song by name<br/>
+`bundle exec download_m4a.rb "Titanic Song"`<br/>
 Download from a youtube link<br/>
-`./downloadMP3.rb https://www.youtube.com/watch?v=bM7SZ5SBzyY`<br/>
+`bundle exec download_m4a.rb https://www.youtube.com/watch?v=bM7SZ5SBzyY`<br/>
 <br/>
-Download multiple songs from a file containing several url<br/>
-`./downloadMP3.rb example_links.txt`<br/>
+Download multiple songs from a file containing several youtube urls or song names (one per line)<br/>
+`bundle exec download_m4a.rb example_links.txt`<br/>
 <br/>
-All the .mp3 files will be sotred into the folder named "Music" (will be created if it doesn't exist)
+All the .m4a files will be stored into the folder named "~/Music" (will be created if it doesn't exist). You can change the folder name by changing the DOWNLOAD_DIRECTORY constant value on download_m4a.rb:9
 
-## Example
-
-![alt text](screenshot/screenshot.png "Screenshot")
-
+## Original Author
+Based on https://github.com/ggouzi/Youtube-Download-Songs, only changed it to download songs by name and in m4a format
 
 ## Licence
 This project is licensed under the GNU GPL v2. See GPL.txt for details.
---
+++ download_m4a.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'nokogiri'
+require 'youtube-dl.rb'
+require 'open-uri'
+
+DOWNLOAD_DIRECTORY = '~/Music/'.freeze
+
+def download(url, name)
+  location = DOWNLOAD_DIRECTORY + name.strip + '.m4a'
+  # Download options
+  options = {
+    format: 'm4a',
+    output: location
+  }
+  puts "DOWNLOADING #{url}/#{name}"
+  YoutubeDL.download url, options
+  puts "DONE (see the folder '#{DOWNLOAD_DIRECTORY}')"
+end
+
+def parseURL(url = nil)
+  if url.include?('youtube.com')
+    url = url.strip
+  else
+    search_url = "https://www.youtube.com/results?search_query=#{url.tr(' ', '+')}"
+    results = open(search_url).read
+    if results =~ /watch\?v=([-\w_]+)/
+      url = "https://www.youtube.com/watch?v=#{Regexp.last_match(1)}"
+    else
+      puts "DIDNT FIND ANY RESULTS FOR #{url}"
+      url = nil
+    end
+  end
+
+  url = url.slice(0, url.index('&')) if url.include? '&'
+  url
+end
+
+def parseTitle(title)
+  title = title.slice(0, title.rindex('-')) if title.include? '-'
+  title
+end
+
+def launchDownload(url, compteur, totalDownloads)
+  file = open(url)
+  doc = Nokogiri::HTML(file)
+  # Encoding options
+  encoding_options = {
+    invalid: :replace, # Replace invalid byte sequences
+    undef: :replace, # Replace anything not defined in ASCII
+    replace: '', # Use a blank for those replacements
+    universal_newline: true # Always break lines with \n
+  }
+  title = doc.at_css('title').text.encode(Encoding.find('ASCII'), encoding_options) # Delete non ASCII chars
+  title = parseTitle(title)
+  puts compteur.to_s + '/' + totalDownloads.to_s + '- ' + title
+  download(url, title)
+rescue OpenURI::HTTPError
+  puts "COULDNT DOWNLOAD #{url}"
+end
+
+if ARGV.length != 1 || ARGV[0] == '-h' || ARGV[0] == '--help'
+  puts 'You  should pass one parameter: The URL link or The file containing all youtube songs URL to download'
+  puts 'make sure youve run `bundle install` first to download dependencies and then one of:'
+  puts 'Example1: bundle exec download_m4a.rb test.txt'
+  puts 'Example2: bundle exec download_m4a.rb https://www.youtube.com/watch?v=bM7SZ5SBzyY'
+  puts 'Example3: bundle exec download_m4a.rb "Titanic Song"'
+  puts
+  exit 0
+end
+
+if ARGV[0].include?('.txt')
+  filename = ARGV[0]
+  file = File.open(filename)
+  total_lines = File.open(filename).readlines.size
+  File.open(filename).each_line.with_index do |line, line_number|
+    launchDownload(parseURL(line), line_number + 1, total_lines)
+  end
+  puts 'FINISHED'
+else
+  launchDownload(parseURL(ARGV[0]), 1, 1)
+end
