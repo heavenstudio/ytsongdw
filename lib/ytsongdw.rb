df7600bd34da9380f13cbc73e2b21911b30284fe
Stefano Diem Benatti <stefano@heavenstudio.com.br>
2017-01-05 04:33:22 -0200

    fixing gem structure

---
+++ lib/ytsongdw.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'open-uri'
+require 'simple_logger'
+require 'counter'
+require 'file_settings'
+require 'downloader'
+require 'playlist_downloader'
+require 'song_downloader'
+require 'youtube_song_downloader'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'simplecov'
 SimpleCov.start
 
-require_relative '../ytsongdw'
+require 'ytsongdw'
 Bundler.require(:default, :test)
